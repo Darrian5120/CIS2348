7b7e3a5f3a648f4757c0aaa98742d21044834e34
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2020-12-02 17:29:54 -0600

    damage, past

--- FinalProject/main1.py
+++ FinalProject/main1.py
@@ -1,25 +1,19 @@
 import os
 import glob
-import xlwt
-import xlrd
 import csv
-import pandas
-import pandas as pd
-import numpy as np
-from collections import defaultdict
-import pprint
 import datetime
 from datetime import date
+from datetime import datetime 
 import operator
 
 class Items:
-    def __init__(self, item_id = 0, manfac_name = 'none', item_type = 'none', damage = '', price = 0, date = 'none'):
+    def __init__(self, item_id = 0, manfac_name = 'none', item_type = 'none', price = 0, date = 'none', damage = ''):
         self.item_id = item_id
         self.manfac_name = manfac_name
         self.item_type = item_type
-        self.damage = damage
         self.price = price
         self.date = date
+        self.damage = damage
     
     def __repr__(self):
         return f"""
@@ -46,7 +40,7 @@ if __name__ == "__main__":
     prices = []
     dates = []
 
-##################### Create list of lists ########################
+##################### Create list of lists ################################
     with open('ManufacturerList.csv','r') as f:
         reader = csv.reader(f, delimiter=',') # read rows into a dictionary format
         row_num = 1
@@ -61,28 +55,61 @@ if __name__ == "__main__":
         read_in(dates, item)
 
     finalized_items = sorted(item, key =lambda l:l[1], reverse=False)
-    
-#################### Create full inventory CSV file ####################
+    for i in range(lines):
+        finalized_items[i].append(finalized_items[i].pop(3))
+
+#################### Create full inventory CSV file ##########################
     with open('FullInventory.csv', 'w', newline='') as f:
         write = csv.writer(f)
         write.writerows(finalized_items)
 
-################## Create past service Inventory CSV file ####################
+################## Create past service Inventory CSV file #####################
     past = []
-    d1 = datetime.datetime.now()
+    d1 = datetime.now()
     for i in range(lines):
-        if (datetime.datetime.strptime(finalized_items[i][5],'%m/%d/%Y')<d1):
+        if (datetime.strptime(finalized_items[i][4],'%m/%d/%Y')<d1):
             past.append(finalized_items[i])
-
+    past = sorted(past, key =lambda x: datetime.strptime(x[4],'%m/%d/%Y'))
     with open('PastServiceDateInventory.csv', 'w', newline='') as f:
         write = csv.writer(f)
         write.writerows(past)
 
+#################### Create damaged inventory list #############################
+    damaged = []
+    for i in range(lines):
+        if (finalized_items[i][5] == 'damaged'):
+            damaged.append(finalized_items[i])
+    damaged = sorted(damaged, key =lambda l:l[3], reverse=True)   
     
+    with open('DamagedInventory.csv', 'w', newline='') as f:
+        write = csv.writer(f)
+        write.writerows(damaged)
+
+###################### Create Item Type Inventory list #########################
+    typeList = []
+    files = []
+    amt = 0
+    filename = '{}Inventory.csv'
+    for i in range(lines):
+        if finalized_items[i][2] not in typeList:
+            typeList.append(finalized_items[i][2])
+            files.append(filename.format(finalized_items[i][2]))
+            amt+=1
     
+    print(typeList)
+    print(files)
+
+    for i in range(lines):
+        for j in range(amt):
+            if (finalized_items[i][2] == typeList[j]):
+                None
 
 
-#################### create class objects ####################
+
+
+
+
+#################### create class objects ######################################
     objs = list()
     for i in range(lines):
         objs.append(Items())
@@ -91,9 +118,9 @@ if __name__ == "__main__":
         objs[i].item_id = int(finalized_items[i][0])
         objs[i].manfac_name = finalized_items[i][1]
         objs[i].item_type = finalized_items[i][2]
-        objs[i].damage = finalized_items[i][3]
-        objs[i].price = int(finalized_items[i][4])
-        objs[i].date = finalized_items[i][5]
+        objs[i].price = int(finalized_items[i][3])
+        objs[i].date = finalized_items[i][4]
+        objs[i].damage = finalized_items[i][5]
 
     ## create dicitonary with keys as item_id and values as class objects ##
     item_dict = {}
