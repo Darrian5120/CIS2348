6eb2375d6e14df47e5e1c53a773112b1a1f639d5
Darrian5120 <dmwoodar@cougarnet.uh.edu>
2020-12-04 11:03:25 -0600

    add name

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -6,6 +6,10 @@ from datetime import date
 from datetime import datetime 
 import operator
 
+# Darrian Woodard
+# 1593984
+# 12/4/20
+
 class Items:
     def __init__(self, item_id = 0, manfac_name = 'none', item_type = 'none', price = 0, date = 'none', damage = ''):
         self.item_id = item_id
@@ -24,21 +28,22 @@ def read_in(x, item):
     reader = csv.reader(f, delimiter=',') # read rows into a dictionary format
     row_num = 1
     for row in reader:
-        x.append(row)
+        x.append(row) # add csv row to a list index
         row_num+=1
     for i in range(lines):
         for j in range(lines): #if prices or dates missing change lines to range(len(x))
-            if (item[i][0] == x[j][0]):
+            if (item[i][0] == x[j][0]): # compare ids to ensure data is added to correct id
                 item[i].append(x[j][1])
 
 if __name__ == "__main__":
     lines = 0
+    # find amt of rows in csv file
     for row in open('ManufacturerList.csv'):
         lines += 1
     
-    item = [] 
-    prices = []
-    dates = []
+    item = [] # list for manufacturer
+    prices = [] # list for prices
+    dates = [] #list for service dates
 
 ##################### Create list of lists ################################
     with open('ManufacturerList.csv','r') as f:
@@ -54,67 +59,68 @@ if __name__ == "__main__":
     with open('ServiceDatesList.csv','r') as f:
         read_in(dates, item)
 
-    finalized_items = sorted(item, key =lambda l:l[1], reverse=False)
+    finalized_items = sorted(item, key =lambda l:l[1], reverse=False) # sort data by manufacturer
     for i in range(lines):
-        finalized_items[i].append(finalized_items[i].pop(3))
+        finalized_items[i].append(finalized_items[i].pop(3)) # move damage to the end of csv row
 
 #################### Create full inventory CSV file ##########################
     with open('FullInventory.csv', 'w', newline='') as f:
         write = csv.writer(f)
-        write.writerows(finalized_items)
+        write.writerows(finalized_items) # write list to csv
 
 ################## Create past service Inventory CSV file #####################
-    past = []
-    d1 = datetime.now()
+    past = [] # past date list
+    d1 = datetime.now() # find current date
     for i in range(lines):
-        if (datetime.strptime(finalized_items[i][4],'%m/%d/%Y')<d1):
-            past.append(finalized_items[i])
-    past = sorted(past, key =lambda x: datetime.strptime(x[4],'%m/%d/%Y'))
+        if (datetime.strptime(finalized_items[i][4],'%m/%d/%Y')<d1): # compare dates
+            past.append(finalized_items[i]) # add to the list
+    past = sorted(past, key =lambda x: datetime.strptime(x[4],'%m/%d/%Y')) # sort based on dates
     with open('PastServiceDateInventory.csv', 'w', newline='') as f:
         write = csv.writer(f)
-        write.writerows(past)
+        write.writerows(past)  # write list to csv
 
 #################### Create damaged inventory list #############################
     damaged = []
     for i in range(lines):
-        if (finalized_items[i][5] == 'damaged'):
-            damaged.append(finalized_items[i])
-    damaged = sorted(damaged, key =lambda l:l[3], reverse=True)   
+        if (finalized_items[i][5] == 'damaged'): # check to see if item is damaged
+            damaged.append(finalized_items[i]) # add to the list
+    damaged = sorted(damaged, key =lambda l:l[3], reverse=True) #sort based on price
     
     with open('DamagedInventory.csv', 'w', newline='') as f:
         write = csv.writer(f)
-        write.writerows(damaged)
+        write.writerows(damaged) # write list to csv
 
 ###################### Create Item Type Inventory list #########################
-    typeList = []
-    files = []
-    amt = 0
-    filename = '{}Inventory.csv'
+    typeList = [] # empty list of item types
+    files = [] # empty list of possible files
+    amt = 0 # variable to store amount of types
+    filename = '{}Inventory.csv' # filename naming convention
     for i in range(lines):
-        if finalized_items[i][2] not in typeList:
+        if finalized_items[i][2] not in typeList: # add different types to list
             typeList.append(finalized_items[i][2])
             files.append(filename.format(finalized_items[i][2]))
             amt+=1
     
-    fullFiles = [[] for i in range(amt)]
+    fullFiles = [[] for i in range(amt)] # create empty list of lists of list ex. [[[],[]],[[],[]]]
 
-    for i in range(amt):
+    for i in range(amt): # add type as one list, and then add the items as list inside that list
         for j in range(lines):
             if (typeList[i] == finalized_items[j][2]):
                 fullFiles[i].append(finalized_items[j])
     
     
     for i in range(amt):
-        fullFiles[i] = sorted(fullFiles[i], key=lambda l:l[0], reverse=False)
+        fullFiles[i] = sorted(fullFiles[i], key=lambda l:l[0], reverse=False) # sort by item id
         with open(files[i], 'w', newline='') as f:
             write = csv.writer(f)
-            write.writerows(fullFiles[i])
+            write.writerows(fullFiles[i]) # write list to csv
 
 #################### create class objects ######################################
-    objs = list()
+    objs = list() #Create empty list
     for i in range(lines):
-        objs.append(Items())
+        objs.append(Items()) # add empty objects to list
 
+    # add items to each class object
     for i in range(lines):
         objs[i].item_id = int(finalized_items[i][0])
         objs[i].manfac_name = finalized_items[i][1]
@@ -124,13 +130,13 @@ if __name__ == "__main__":
         objs[i].damage = finalized_items[i][5]
 
     ## create dicitonary with keys as item_id and values as class objects ##
-    item_dict = {}
+    item_dict = {} # create empty dict
     for i in range(lines):
-        item_dict[objs[i].item_id] = None
+        item_dict[objs[i].item_id] = None # add keys w/ no values
     for key in item_dict:
         for i in range(lines):
-            if (key == objs[i].item_id):
-                item_dict[key] = objs[i] 
+            if (key == objs[i].item_id): #compare ids
+                item_dict[key] = objs[i] # add if ids match
 
 ############################### Menu ###########################################
     command = ''
